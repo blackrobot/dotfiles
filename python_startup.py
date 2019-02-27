a1639d859d286954dac205ef1b8b16d6ef38ab03
Damon Jablons <damonjablons@gmail.com>
2019-02-27 14:20:41 -0500

    Clean up module imports

--- python_startup.py
+++ python_startup.py
@@ -1,9 +1,29 @@
 # flake8: noqa
 
-# Default imports
-import datetime as dt
-import pathlib
-import os
+import importlib
 import sys
 
-print("\n>>> import datetime as dt, pathlib, os, sys\n")
+
+def import_to_namespace(name, package=None, alias=None):
+    if not alias:
+        alias = name.partition(".")[0]
+    sys.modules[alias] = importlib.import_module(name, package=package)
+    globals()[alias] = sys.modules[alias]
+
+
+modules_to_import = (
+    ("datetime",),
+    ("pathlib",),
+    ("os",),
+)
+
+
+for import_args in modules_to_import:
+    import_to_namespace(*import_args)
+
+
+print("\n* imported: {}\n".format(", ".join(x for x, *_ in modules_to_import)))
+
+
+# cleanup
+del import_to_namespace, modules_to_import, importlib
