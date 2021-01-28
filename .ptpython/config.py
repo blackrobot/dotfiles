566f92d9dcd5d655a727ca21b1e4127aafe8d9e1
Damon Jablons <damonjablons@gmail.com>
2021-01-28 16:24:40 -0500

    Latest config options for ptpython

--- .ptpython/config.py
+++ .ptpython/config.py
@@ -1,11 +1,14 @@
 """
-The example configuration used can be found here: https://git.io/fjgyb
+The example configuration used can be found here:
+https://github.com/prompt-toolkit/ptpython/blob/7ea2e5b/examples/ptpython_config/config.py
 """
 
-from __future__ import unicode_literals
+from __future__ import unicode_literals, annotations
+
 import os
 
 from prompt_toolkit.keys import Keys
+from ptpython.repl import PythonRepl
 from ptpython.layout import CompletionVisualisation
 
 
@@ -19,13 +22,13 @@ inside_of_vim = os.getenv("VIMRUNTIME", False)
 #    1 = "DEPTH_1_BIT"
 #    4 = "DEPTH_4_BIT"
 #    8 = "DEPTH_8_BIT"
-#   24 = "DEPTH_24_BIT"  ->  Can't get truecolor to work properly :(
-color_depth = "DEPTH_8_BIT"
+#   24 = "DEPTH_24_BIT"
+COLOR_DEPTH = "DEPTH_24_BIT"
 
 # Styles
 # ------
 # To see the options for styles use:
-#     python -c 'from pygments.styles import STYLE_MAP as M; print("\n".join(sorted(M)))'
+#     python -c 'from pygments.styles import STYLE_MAP as M; print("\n".join(sorted(M)))'  # noqa
 #
 # Or look at the appropriate version of this file:
 # https://bitbucket.org/birkenfeld/pygments-main/src/40f3be5ba01a09703bd040e189977d1cac735458/pygments/styles/__init__.py#lines-17:53
@@ -61,7 +64,7 @@ code_colorscheme = "native"
 # "xcode"
 
 
-def configure(repl):
+def configure(repl: PythonRepl):
     """
     Configuration method. This is called during the start-up of ptpython.
 
@@ -93,6 +96,9 @@ def configure(repl):
     # When the sidebar is visible, also show the help text.
     repl.show_sidebar_help = True
 
+    # Swap light/dark colors on or off
+    repl.swap_light_and_dark = False
+
     # Highlight matching parethesis.
     repl.highlight_matching_parenthesis = True
 
@@ -102,6 +108,13 @@ def configure(repl):
     # Complete while typing. (Don't require tab before the completion
     # menu is shown.). See `repl.enable_history_search`...
     repl.complete_while_typing = True
+
+    # Fuzzy and dictionary completion
+    repl.enable_dictionary_completion = True
+    # NOTE: Setting this to true can make the completer lag the terminal
+    # session if there are lots of completions. This is especially the
+    # case with a lot of chaining, eg: Product.object.filter().etc(...)
+    repl.enable_fuzzy_completion = False
 
     # Mouse support.
     repl.enable_mouse_support = False
@@ -152,11 +165,21 @@ def configure(repl):
 
     # Enable 24bit True color. (Not all terminals support this. -- maybe check
     # $TERM before changing.)
-    repl.color_depth = color_depth
+    repl.color_depth = COLOR_DEPTH
+
+    # Min/max brightness
+    repl.min_brightness = 0.0  # Increase for dark terminal backgrounds.
+    repl.max_brightness = 1.0  # Decrease for light terminal backgrounds.
 
     repl.enable_syntax_highlighting = True
 
-    @repl.add_key_binding(Keys.ControlSpace)  # "c-space" | "c-@"
+    # Get into Vi navigation mode at startup
+    repl.vi_start_in_navigation_mode = False
+
+    # Preserve last used Vi input mode between main loop iterations
+    repl.vi_keep_last_used_mode = False
+
+    @repl.add_key_binding("c-space")  # "c-space" | "c-@"
     def trigger_autosuggest(event):
         buff = event.current_buffer
         # buff = event.cli.current_buffer
